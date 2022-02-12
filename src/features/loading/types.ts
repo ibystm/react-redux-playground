ada123a337dcf770f13e6617411c8c6437e993a4
Kosuke Takahashi <rhcp.kdog@gmail.com>
2022-02-13 01:30:15 +0900

    remove annecessary type definitions.

--- src/features/loading/types.ts
+++ src/features/loading/types.ts
@@ -2,14 +2,3 @@ export type Loading = {
   loading: boolean;
   message: string;
 };
-
-export type StartLoadingAction = {
-  type: "START_LOADING";
-  payload: string;
-};
-
-export type StopLoadingAction = {
-  type: "STOP_LOADING";
-};
-
-export type LoadingActions = StartLoadingAction | StopLoadingAction;
